acc5d02526f462dd81a296a56e4c8b84863e9f08
Jared Pavlecic <jdp313@gmail.com>
2019-10-21 15:04:09 -0400

    updated homework

--- module-2/06_Database_Connectivity_DAO/pair-exercise/java/src/main/java/com/techelevator/projects/model/jdbc/JDBCProjectDAO.java
+++ module-2/06_Database_Connectivity_DAO/pair-exercise/java/src/main/java/com/techelevator/projects/model/jdbc/JDBCProjectDAO.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import com.techelevator.projects.model.Project;
 import com.techelevator.projects.model.ProjectDAO;
@@ -20,17 +21,39 @@ public class JDBCProjectDAO implements ProjectDAO {
 	
 	@Override
 	public List<Project> getAllActiveProjects() {
-		return new ArrayList<>();
+		ArrayList<Project> projects = new ArrayList<> ();
+		String sqlAllProjects = "SELECT project_id, name,from_date, to_date" +
+								"FROM project";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlAllProjects);
+		while(results.next()) {
+			Project project = mapRowToProject(results);
+			projects.add (project);
+			
+			
+		}
+		return projects;
 	}
 
 	@Override
 	public void removeEmployeeFromProject(Long projectId, Long employeeId) {
-		
+		String sqlDelete = "DELETE from project_employee WHERE project_id = ? AND employee_id = ?";
+		jdbcTemplate.update(sqlDelete, projectId, employeeId);
 	}
 
 	@Override
 	public void addEmployeeToProject(Long projectId, Long employeeId) {
+		String sqlInsert = "INSERT INTO project_employee (proeject_id, employee_id) VALUES (?,?)";
+		jdbcTemplate.update(sqlInsert, projectId, employeeId);
+	}
+	
+	private Project mapRowToProject(SqlRowSet results) {
+		Project projects = new Project();//making a pojo
+		projects.setId(results.getLong("project_id"));
+		projects.setName(results.getString("name"));
+		projects.setStartDate(results.getDate("from_date").toLocalDate());
+		projects.setEndDate(results.getDate("to_date").toLocalDate());
 		
+		return projects;
 	}
 
 }
