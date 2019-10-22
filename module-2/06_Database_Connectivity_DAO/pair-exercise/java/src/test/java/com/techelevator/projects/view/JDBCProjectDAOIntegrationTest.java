56395cd01d9fbdce11abf37c1582ddc830e01ee9
Dan Podobnik <daniel.podobnik@gmail.com>
2019-10-22 15:31:30 -0400

    provided comments

--- module-2/06_Database_Connectivity_DAO/pair-exercise/java/src/test/java/com/techelevator/projects/view/JDBCProjectDAOIntegrationTest.java
+++ module-2/06_Database_Connectivity_DAO/pair-exercise/java/src/test/java/com/techelevator/projects/view/JDBCProjectDAOIntegrationTest.java
@@ -14,7 +14,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 
-import com.techelevator.city.City;
+
 import com.techelevator.projects.model.Employee;
 import com.techelevator.projects.model.Project;
 import com.techelevator.projects.model.jdbc.JDBCEmployeeDAO;
@@ -56,6 +56,9 @@ public class JDBCProjectDAOIntegrationTest {
 	public void getting_active_new_projects() {
 		// create helper method that inserts new project into database
 		List<Project> oldProjects = dao.getAllActiveProjects();// using the object created above to test
+		//              ^---- contains the old projects, let's say there's 3 old projects
+		
+		// we'll add a new active project into the database directly without the DAO
 		String sqlInsert = "INSERT INTO project (name, from_date, to_date) VALUES(?,?,?)";
 		JdbcTemplate jdbcTemplate = new JdbcTemplate(projects);
 		jdbcTemplate.update(sqlInsert, "Project Veritas", Date.valueOf("2009-01-25"), Date.valueOf("2030-4-24"));// this
@@ -70,32 +73,61 @@ public class JDBCProjectDAOIntegrationTest {
 																													// test
 																													// now
 		List<Project> newProjects = dao.getAllActiveProjects();
+		//              ^---- contains the new project list, hopefully with our newly added project
+		//					  If oldProjects was 3 we're hoping it's 4 now
 		Assert.assertEquals(oldProjects.size() + 1, newProjects.size());
-
 	}
 
 	@Test
 	public void remove_employee_from_project() {
+		// ARRANGE
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(projects); // our own "SQL Commander"
+		// make our query, put question marks for the values
 		String sqlInsertProject = "INSERT INTO project (project_id, name, from_date, to_date) VALUES(?,?,?,?)";
+		// get the project ID
 		long project_id = getNextProjectId();
+		// run the query
+		jdbcTemplate.update(sqlInsertProject,project_id, "Project Unicorn",Date.valueOf("2009-01-25"),Date.valueOf("2030-4-24"));
+		
+		// repeat the above with employee
 		String sqlInsertEmployee = "INSERT INTO employee (employee_id,first_name,last_name,birth_date,gender,hire_date) VALUES(?,?,?,?,?,?)";
 		long employee_id = getNextEmployeeId();
-		JdbcTemplate jdbcTemplate = new JdbcTemplate(projects);
-		jdbcTemplate.update(sqlInsertProject,project_id, "Project Unicorn",Date.valueOf("2009-01-25"),Date.valueOf("2030-4-24"));
 		jdbcTemplate.update(sqlInsertEmployee, employee_id, "Mark", "Jones", Date.valueOf("1975-05-10"), 'M', Date.valueOf("2008-03-11"));
+		
+		
+		
+		
+		// ACT
+		// ask the DAO to assign the employee to the project using our new employee_id and project_id
 		dao.addEmployeeToProject(project_id, employee_id);
+		
+		
+		// ASSERT
+		// now let's double check it
+		// we make a new employee dao
 		JDBCEmployeeDAO newEmployee = new JDBCEmployeeDAO(projects);
+		// a list of all the employees named Mark Jones
 		List<Employee> newPersonList = newEmployee.searchEmployeesByName("Mark", "Jones");
+		// a list of all employees on the project project_id
 		List<Employee> newProjectList = newEmployee.getEmployeesByProjectId(project_id);
+		// make sure those two employees are the same person
 		assertEmployeesAreEqual(newPersonList.get(0), newProjectList.get(0));
+		
+		
+		
+		
+		
+		
+		
 		dao.removeEmployeeFromProject(project_id, employee_id);
 		newProjectList = newEmployee.getEmployeesByProjectId(project_id);
 		Assert.assertEquals(0, newProjectList.size());
 	}
 
 	private long getNextProjectId() {
-		JdbcTemplate jdbcTemplate = new JdbcTemplate(projects);
-		SqlRowSet nextIdResult = jdbcTemplate.queryForRowSet("SELECT nextval('seq_project_id')");
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(projects); // SQL Commander
+		String sql = "SELECT nextval('seq_project_id')"; // query
+		SqlRowSet nextIdResult = jdbcTemplate.queryForRowSet(sql);
 		if (nextIdResult.next()) {
 			long nextID = nextIdResult.getLong(1);
 			return nextID;
